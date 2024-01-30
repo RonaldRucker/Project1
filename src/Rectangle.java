3f793288cc7c949e93fa6b5050a03b36cab33c45
RonaldRucker <154106899+RonaldRucker@users.noreply.github.com>
2024-01-30 10:27:53 -0500

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -136,10 +136,10 @@ public class Rectangle {
      *         rectangle
      */
     public String toString() {
-        return ", " + xCoordinate +
-               ", " + yCoordinate +
-               ", " + height +
-               ", " + width + ")";
+        return xCoordinate + ", " +
+               yCoordinate + ", " + 
+               height + ", " + 
+               width + ")";
     }
 
 
